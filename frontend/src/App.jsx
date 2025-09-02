78328798a7709bfa41e000ca6dd741e98b4b927b
Raghavendra <22h51a66e1@cmrcet.ac.in>
2025-09-02 20:47:49 +0530

    fixed the import-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { UserData } from "./context/UserContext";
 import { Loading } from "./components/Loading";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import PinPage from "./pages/PinPage";
 import Create from "./pages/Create";
 import Account from "./pages/Account";
